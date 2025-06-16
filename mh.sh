5d68cfd6869693a87cac933a02116eacd0a05697
Viktor Symov <vsimov@yandex.ru>
2025-06-16 09:41:06 +0200

    Upload project

---
+++ mh.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+# Help for Linux commands
+
+
+DIR=`dirname $0`
+
+Command_list=`find $DIR/data -type f -name "*" -exec basename \{} .txt \; | sort`
+
+
+
+if [ $# -eq 0 ]
+then
+    echo "The Help supports arguments:"
+    for element in $Command_list
+    do
+	printf " # hl %s\n" $element
+    done
+    echo ""
+    exit 0
+fi
+
+
+if [ $# -eq 1 ]
+then
+    cat $DIR/data/$1.txt
+    echo ""
+    exit 0
+fi
+
